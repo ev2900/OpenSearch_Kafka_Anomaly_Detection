c89d01244ce8153cee5fda37b820415acc6a7dd2
Christopher Sharkey <ev2900@gmail.com>
2022-04-17 17:31:10 -0400

    added batch upload code

---
+++ Lambda/1_lambda_function_code_batch.py
@@ -0,0 +1,77 @@
+import json
+import base64
+import re
+import urllib3
+import time
+import re
+
+def lambda_handler(event, context):
+       
+    # Decode Kafka message(s)
+    partition0 = event['records']
+    messages_raw = partition0['ApplicationMetricTopic-0']
+
+    print('Batch size = ' + str(len(messages_raw)) + ' messages')
+
+    bulk_documents_r_body = ''
+
+    for message_raw in messages_raw:
+
+        topic = message_raw['topic']
+        partition = message_raw['partition']
+        offset = message_raw['offset']
+        timestamp = message_raw['timestamp']
+        timestampType = message_raw['timestampType']
+        value = message_raw['value']
+                            
+        decoded_b64_message_value = base64.b64decode(value)
+        decoded_utf8_message_value = decoded_b64_message_value.decode("utf-8")
+                            
+        decoded_utf8_message_value_dict = json.loads(decoded_utf8_message_value.replace("'", "\""))
+            
+        # Send message(s) to OpenSearch
+            
+        http = urllib3.PoolManager()
+        
+        os_url = 'https://search-workshop-domain-cngz2xx47dfl3gkafr7i3ih3ry.us-east-1.es.amazonaws.com'
+        
+        insert_document_r_body = {
+            "eventtime": decoded_utf8_message_value_dict['eventtime'],
+            "application_id": decoded_utf8_message_value_dict['application_id'],
+            "cpu_util": int(decoded_utf8_message_value_dict['cpu_util']),
+            "memory_util": int(decoded_utf8_message_value_dict['memory_util']),
+            "disk_util": int(decoded_utf8_message_value_dict['disk_util'])
+        }
+            
+        meta_data = {
+        "index": 
+          {
+            "_id": str(decoded_utf8_message_value_dict['message_id'])
+          }
+        }
+            
+        bulk_documents_r_body = str(bulk_documents_r_body) + str(meta_data) + str('\n')
+        bulk_documents_r_body = str(bulk_documents_r_body) + str(insert_document_r_body) + str('\n') 
+        
+    full_url = os_url + '/infa-logs-1/_bulk/'
+        
+    auth_header = urllib3.make_headers(basic_auth='OSMasterUser:AwS#OpenSearch1')
+    
+    bulk_documents_r_body = bulk_documents_r_body.replace("'", "\"")
+    
+    resp = http.request(
+            'POST',
+            full_url,
+            body=bulk_documents_r_body,
+            headers={'Content-Type': 'application/x-ndjson', 'authorization': auth_header['authorization']}
+    )
+    
+    print('Request sent!')
+    print('Response status = ' + str(resp.status))
+    #print(resp.headers)
+    print('Response body = ' + str(resp.data.decode('utf-8')))
+ 
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Success')
+    }
