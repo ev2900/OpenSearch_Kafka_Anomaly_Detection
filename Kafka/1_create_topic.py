b5e40248e2f964f03ea62f3716dcfd834ccc19db
Christopher Sharkey <ev2900@gmail.com>
2022-06-05 13:09:40 -0500

    Update 1_create_topic.py

--- Kafka/1_create_topic.py
+++ Kafka/1_create_topic.py
@@ -1,5 +1,5 @@
 '''
-1. sudo yum install java-1.8.0
+1. yes | sudo yum install java-1.8.0
 2. wget https://archive.apache.org/dist/kafka/2.6.2/kafka_2.12-2.6.2.tgz
 3. tar -xzf kafka_2.12-2.6.2.tgz
 4. cd kafka_2.12-2.6.2
