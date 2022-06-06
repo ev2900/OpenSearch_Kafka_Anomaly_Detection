3718df4345597b6aced9227ee9f869a7b1292a4e
Christopher Sharkey <ev2900@gmail.com>
2022-06-06 15:03:15 -0400

    added map IAM user script

---
+++ OpenSearch/2_map_IAM_user.py
@@ -0,0 +1,31 @@
+# --------------
+# Prerequisite
+# --------------
+# If you have not already installed the requests package and/or the json package 
+# a. pip install requests
+# b. pip install json
+
+import requests
+import json
+
+# --------------
+# Step 1 - Update this URL with your domain endpoint
+# --------------
+os_url = '<os_domain_url>'
+
+# --------------
+# Step 2 - Update ARN of IAM role to map to OpenSearch all_access role
+# --------------
+
+iam_arn = '<IAM_user/role_arn'
+
+# --------------
+# Step 3 - Map IAM user ARN to all_access role
+# --------------
+request_body = {
+	"users" : [iam_arn, "OSMasterUser"]
+}	
+
+map_user_r = requests.put(os_url + '/_plugins/_security/api/rolesmapping/all_access', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(request_body))
+
+print(map_user_r.text)
