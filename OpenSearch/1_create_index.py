a8985251b7cdd49d0d2f9d1a9a3f61259d936739
Christopher Sharkey <ev2900@gmail.com>
2022-06-22 12:24:49 -0500

    Update 1_create_index.py

--- OpenSearch/1_create_index.py
+++ OpenSearch/1_create_index.py
@@ -11,6 +11,7 @@ import json
 # Step 1 - Update this URL with your domain endpoint
 # --------------
 os_url = '<opensearch_domain_endpoint_url>'
+os_url_clean = os_url.rstrip("/")
 
 # --------------
 # Step 2
@@ -44,7 +45,7 @@ create_index_r_body = {
   }
 }
 
-create_index_r = requests.put(os_url + '/infa-logs-1', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(create_index_r_body))
+create_index_r = requests.put(os_url_clean + '/infa-logs-1', auth=('OSMasterUser', 'AwS#OpenSearch1'), headers= {'Content-type': 'application/json'}, data=json.dumps(create_index_r_body))
 
 print('------ Created an index on os cluster ------')
 print(create_index_r.text)
